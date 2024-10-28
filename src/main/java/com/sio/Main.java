645328216e3698c80144da5a2fe1217a655a4ab3
Elonedayan <Elone.DAYAN@ortmontreuil.fr>
2024-10-28 17:25:14 +0100

    evalelone

--- src/main/java/com/sio/Main.java
+++ src/main/java/com/sio/Main.java
@@ -1,6 +1,7 @@
 package com.sio;
 
 import java.text.DecimalFormat;
+import java.text.MessageFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -27,14 +28,23 @@ public class Main {
             int choice = scanner.nextInt();
             scanner.nextLine();
 
-            switch (choice){
+            switch (choice) {
                 case 1:
                     System.out.println("---------------------------------------------");
                     System.out.println("Exercice 1");
                     System.out.println("---------------------------------------------");
                     //Exercice 1 : Entrer votre code ici
 
-
+                    System.out.println("Entrer un nombre svp: ");
+                    int nombreSvp = scanner.nextInt();
+                    System.out.println("Entrer une puissance svp: ");
+                    int puissanceSvp = scanner.nextInt();
+                    if (puissanceSvp >0);
+                    System.out.println("La puissance doit être supérieur ou égal à 0");
+                    System.out.println("Entrer une puissance svp: ");
+                    int puissanceSvp2 = scanner.nextInt();
+                    double resultat = Math.pow(nombreSvp, puissanceSvp);
+                    System.out.println("Le résultat est " + resultat);
 
                     System.out.println("---------------------------------------------");
                     break;
@@ -44,6 +54,29 @@ public class Main {
                     System.out.println("---------------------------------------------");
                     //Exercice 2 : Entrer votre code ici
 
+
+                    System.out.println("Entrer le nombre de kilomètres parcourues par le client svp: ");
+                    int nombreKm1 = scanner.nextInt();
+                    if (nombreKm1 <0) ;
+                    System.out.println("Le kilomètes doit être supérieur ou égale à 0" );
+
+                    System.out.println("Entrer le nombre de kilomètres parcourues par le client svp: ");
+
+                     int nombreKm = scanner.nextInt();
+                    if (nombreKm < 100) {
+                        double prix = 0.20 * nombreKm;
+                        System.out.println("Le montant à payer par le client est de :" + prix);
+                    }
+
+                    if (nombreKm > 100 && nombreKm < 500) {
+                    double prix = 0.50 * nombreKm;
+                    System.out.printf("Le montant à payer par le client est de :%s%n", prix);
+
+                    }
+                     if (nombreKm >500) {
+                         double prix = 0.80 * nombreKm;
+                        System.out.println("Le montant à payer par le client est de :" + prix);
+                     }
 
 
 
@@ -66,16 +99,18 @@ public class Main {
                     System.out.println("Exercice 4");
                     System.out.println("---------------------------------------------");
                     //Exercice 4 : Entrer votre code ici
-
-
-
+                    System.out.println("Entrer un nombre svp");
+                    int nombreSvp2 = scanner.nextInt();
 
                     System.out.println("---------------------------------------------");
                     break;
+
                 case 0:
                     System.out.println(RESET);
                     System.exit(0);
                     break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + choice);
             }
         }
     }
